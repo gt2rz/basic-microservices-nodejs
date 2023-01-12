425b06b1d74403b09fbfc275742092a568143ca8
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-01-12 16:58:05 -0500

    add middlewares and helmet

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,8 +1,28 @@
 import gateway from "fast-gateway";
+import rateLimit from "express-rate-limit";
+import requestIp from "request-ip";
+import helmet from "helmet";
 
 const port = 9001;
 
 const server = gateway({
+  middlewares: [
+    // first acquire request IP
+    (req, res, next) => {
+      req.ip = requestIp.getClientIp(req);
+      return next();
+    },
+    // second enable rate limiter
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 60, // limit each IP to 60 requests per windowMs
+      handler: (req, res) => {
+        res.send("Too many requests, please try again later.", 429);
+      },
+    }),
+
+  ],
+
   routes: [
     {
       prefix: "/orders",
@@ -15,8 +35,10 @@ const server = gateway({
   ],
 });
 
-server.get('/', (req, res) => {
-  res.send('API Gateway running...');
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.send("API Gateway running...");
 });
 
 server.start(port).then(() => {
